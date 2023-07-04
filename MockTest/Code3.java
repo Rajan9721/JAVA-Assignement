1c437582a7b2db5151797610ce230f62d294f754
Rajan9721 <rn638600@gmail.com>
2023-07-05 00:07:17 +0530

    Update

--- MockTest/Code3.java
+++ MockTest/Code3.java
@@ -1,7 +1,21 @@
 package MockTest;
-
+import java.util.Scanner;
 public class Code3 {
     public static void main(String[] args) {
+        int i=0;
+        float sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        float arr[] = new float[n];
+        System.out.println("Enter " + n + " Numbers: ");
+        do{
+            arr[i] = sc.nextInt();
+            sum=sum+arr[i];
+            i++;
+        }while(i<n);
+        float average = sum/n;
+        System.out.println("Average of number is : "+average);
 
     }
 }
